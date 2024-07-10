9133522a97cd28eadc74f5cafc5c12e838673929
nkordis <nikoskordis@gmail.com>
2024-07-11 00:01:07 +0200

    feat: Retrieve and display categories

--- EShop.Web/Controllers/CategoryController.cs
+++ EShop.Web/Controllers/CategoryController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EShop.Web.Data;
+using EShop.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EShop.Web.Controllers;
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public CategoryController(ApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    } 
+
     public IActionResult Index()
     {
-        return View();
+        List<Category> categories = _applicationDbContext.Categories.ToList();
+
+        return View(categories);
     }
 }
 
