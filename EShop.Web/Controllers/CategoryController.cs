2d465a6b72359f16912dcd66c8f75fa4f18565ad
nkordis <nikoskordis@gmail.com>
2024-07-10 12:03:17 +0200

    feat: Add Category controller and configure views

---
+++ EShop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.Controllers;
+public class CategoryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
