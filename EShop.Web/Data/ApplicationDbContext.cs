41b0c5d18a7505109dd8a1b52b3a0b03e726f04d
nkordis <nikoskordis@gmail.com>
2024-07-10 12:55:17 +0200

    chore: Seed initial data into Category table

--- EShop.Web/Data/ApplicationDbContext.cs
+++ EShop.Web/Data/ApplicationDbContext.cs
@@ -6,4 +6,14 @@ namespace EShop.Web.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Designers", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "Clothing", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "Shoes", DisplayOrder = 3 }
+            );
+
+    }
 }
