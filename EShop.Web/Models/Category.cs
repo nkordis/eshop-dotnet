cfb0805af77576d7e4ca6c98709349491057a2f3
nkordis <nikoskordis@gmail.com>
2024-07-13 14:00:48 +0200

    feat: Improve Create Category form with tag helpers and data annotations

--- EShop.Web/Models/Category.cs
+++ EShop.Web/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace EShop.Web.Models;
 
@@ -7,7 +8,9 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required]
+    [DisplayName("Category Name")]
     public string Name { get; set; }
+    [DisplayName("Display Order")]
     public int DisplayOrder { get; set; }
 }
 
