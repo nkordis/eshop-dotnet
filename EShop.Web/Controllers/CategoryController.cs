e9620f999a32f4b2a7110ad51a65ebd3a7ef5892
nkordis <nikoskordis@gmail.com>
2024-07-16 11:41:01 +0200

    feat: Implement edit functionality for categories

--- EShop.Web/Controllers/CategoryController.cs
+++ EShop.Web/Controllers/CategoryController.cs
@@ -28,5 +28,34 @@ public class CategoryController(ApplicationDbContext _applicationDbContext) : Co
 
         return View();
     }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        Category? category = _applicationDbContext.Categories.Find(id);
+
+        if (category == null) 
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+    [HttpPost]
+    public IActionResult Edit(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _applicationDbContext.Categories.Update(category);
+            _applicationDbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
 }
 
