655aa283481aaf99f0182a689a62b19b0d26f22f
nkordis <nikoskordis@gmail.com>
2024-07-18 17:59:15 +0200

    feat: Implement CategoryRepository

---
+++ EShop.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using EShop.DataAccess.Data;
+using EShop.DataAccess.Repository.IRepository;
+using EShop.Models.Models;
+
+namespace EShop.DataAccess.Repository;
+
+public class CategoryRepository(ApplicationDbContext db) 
+    : Repository<Category>(db), ICategoryRepository
+{
+    public void Save()
+    {
+        db.SaveChanges();
+    }
+
+    public void Update(Category category)
+    {
+        db.Update(category);
+    }
+}
+
