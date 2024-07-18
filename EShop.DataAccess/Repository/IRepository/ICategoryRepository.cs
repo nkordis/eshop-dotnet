5ce759d0389315237518661411f9650b75fb240b
nkordis <nikoskordis@gmail.com>
2024-07-18 17:38:07 +0200

    feat: Create ICategoryRepository interface

---
+++ EShop.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using EShop.Models.Models;
+
+namespace EShop.DataAccess.Repository.IRepository;
+
+public interface ICategoryRepository : IRepository<Category>
+{
+    void Update(Category category);
+    void Save();
+}
+
