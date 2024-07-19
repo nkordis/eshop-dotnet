df37de0884a3ede5b08c188459aee792dc3ea274
nkordis <nikoskordis@gmail.com>
2024-07-19 19:23:31 +0200

    feat: Add Product repository and integrate with UnitOfWork

--- EShop.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ EShop.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -4,6 +4,7 @@ namespace EShop.DataAccess.Repository.IRepository;
 public interface IUnitOfWork
 {
     ICategoryRepository Category { get; }
+    IProductRepository Product { get; }
     void Save();
 }
 
---
+++ EShop.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using EShop.DataAccess.Data;
+using EShop.DataAccess.Repository.IRepository;
+using EShop.Models.Models;
+
+namespace EShop.DataAccess.Repository;
+
+public class ProductRepository(ApplicationDbContext db) 
+    : Repository<Product>(db), IProductRepository
+{
+    public void Update(Product product)
+    {
+        db.Update(product);
+    }
+}
