ffe8238c73c7062ce64762bc8cf90d90c8c3135d
nkordis <nikoskordis@gmail.com>
2024-07-19 19:03:49 +0200

    feat: Add Product model with basic properties

---
+++ EShop.Models/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Models.Models;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public string Size { get; set; }
+    [Required]
+    [Display(Name = "List Price")]
+    [Range(1,100000)]
+    public decimal ListPrice { get; set; }
+}
+
