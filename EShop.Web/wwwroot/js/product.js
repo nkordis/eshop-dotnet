a2991db26a7aa9f34fa59897517da1d91610d967
nkordis <nikoskordis@gmail.com>
2024-07-24 11:27:10 +0200

    feat: Implement AJAX request using DataTables.net to load product data into DataTables

---
+++ EShop.Web/wwwroot/js/product.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    $('#tblData').DataTable({
+        "ajax": {
+            "url": "/admin/product/getall",
+            "type": "GET",
+            "datatype": "json",
+            "dataSrc": "data"
+        },
+        "columns": [
+            { "data": "title", "width": "15%" },
+            { "data": "description", "width": "30%" },
+            { "data": "size", "width": "5%" },
+            { "data": "listPrice", "width": "5%" },
+            { "data": "category.name", "width": "10%" }
+        ]
+    });
+}
