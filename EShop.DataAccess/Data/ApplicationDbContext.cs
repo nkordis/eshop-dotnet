35a50f041ff4bb66ef9c02921b8ecd359b6d80dc
nkordis <nikoskordis@gmail.com>
2024-07-25 15:50:30 +0200

    chore: Prepare for identity scaffolding by updating DbContext to IdentityDbContext

--- EShop.DataAccess/Data/ApplicationDbContext.cs
+++ EShop.DataAccess/Data/ApplicationDbContext.cs
@@ -1,15 +1,18 @@
 ﻿using EShop.Models.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EShop.DataAccess.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
 {
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         modelBuilder.Entity<Category>().HasData(
             new Category { Id = 1, Name = "Designers", DisplayOrder = 1 },
             new Category { Id = 2, Name = "Clothing", DisplayOrder = 2 },
