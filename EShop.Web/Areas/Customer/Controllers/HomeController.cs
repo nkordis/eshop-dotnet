e96a59a71d077250482148883e55e1a37051a260
nkordis <nikoskordis@gmail.com>
2024-07-25 10:13:45 +0200

    feat: Display all products on the customer home page using Bootstrap cards

--- EShop.Web/Areas/Customer/Controllers/HomeController.cs
+++ EShop.Web/Areas/Customer/Controllers/HomeController.cs
@@ -1,33 +1,28 @@
+using EShop.DataAccess.Repository.IRepository;
 using EShop.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace EShop.Web.Areas.Customer.Controllers
+namespace EShop.Web.Areas.Customer.Controllers;
+
+[Area("Customer")]
+public class HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork) : Controller
 {
-    [Area("Customer")]
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        private readonly ILogger<HomeController> _logger;
+        List<Product> products = [.. unitOfWork.Product.GetAll(includeProperties: "Category")];
+        return View(products);
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
+
