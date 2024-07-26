e205a6d41880be5cdeda9fff29eb7aa857761112
nkordis <nikoskordis@gmail.com>
2024-07-26 02:00:52 +0200

    chore: Add Role Management to Identity and Seed Default Roles

--- EShop.Utilities/SD.cs
+++ EShop.Utilities/SD.cs
@@ -3,5 +3,9 @@ namespace EShop.Utilities;
 
 public static class SD
 {
+    public const string Role_Customer = "Customer";
+    public const string Role_Company = "Company";
+    public const string Role_Admin = "Admin";
+    public const string Role_Employee = "Employee";
 }
 
