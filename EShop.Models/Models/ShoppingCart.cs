374003985b247e5f9a184c981c3d35eec62683f8
nkordis <nikoskordis@gmail.com>
2024-07-31 17:56:36 +0200

    feat: Add ShoppingCart model and migrations to store shopping cart items in the database

---
+++ EShop.Models/Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EShop.Models.Models;
+
+public class ShoppingCart
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    [ForeignKey(nameof(ProductId))]
+    [ValidateNever]
+    public Product Product { get; set; }
+    public string ApplicationUserId { get; set; }
+    [ForeignKey(nameof(ApplicationUserId))]
+    [ValidateNever]
+    public ApplicationUser ApplicationUser { get; set; }
+}
