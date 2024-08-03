ed7fa774f6781f9c63fdcecaedb348a0850f33ce
nkordis <nikoskordis@gmail.com>
2024-08-03 09:41:36 +0200

    feat: Create ShoppingCart controller, view, and add to layout

---
+++ EShop.Web/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.Areas.Customer.Controllers;
+
+[Area("Customer")]
+public class CartController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
