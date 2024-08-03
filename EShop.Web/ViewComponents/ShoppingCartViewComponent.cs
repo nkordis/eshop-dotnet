78a2b0522d92689eb951cbd078dec61698e49167
nkordis <nikoskordis@gmail.com>
2024-08-03 19:07:17 +0200

    feat: Implement ShoppingCart view component to display cart item count dynamically in navbar

---
+++ EShop.Web/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using EShop.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EShop.Web.ViewComponents;
+
+public class ShoppingCartViewComponent(IUnitOfWork unitOfWork) : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        var cartCount = userId == null ? 0 : unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == userId).Count();
+
+        return View(cartCount);
+    }
+}
