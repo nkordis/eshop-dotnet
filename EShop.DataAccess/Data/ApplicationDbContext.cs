d468215d2784200271554e62c5d61bc30c89fe47
nkordis <nikoskordis@gmail.com>
2024-08-04 11:48:50 +0200

    feat: Add OrderHeader, OrderDetail models and update the db

--- EShop.DataAccess/Data/ApplicationDbContext.cs
+++ EShop.DataAccess/Data/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Company> Companies { get; set; }
     public DbSet<ShoppingCart> ShoppingCarts { get; set; }
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public DbSet<OrderHeader> OrderHeaders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ EShop.Models/Models/OrderDetail.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EShop.Models.Models;
+
+public class OrderDetail
+{
+    public int Id { get; set; }
+    [Required]
+    public int OrderHeaderId { get; set; }
+    [ForeignKey(nameof(OrderHeaderId))]
+    [ValidateNever]
+    public OrderHeader OrderHeader { get; set; }
+
+    [Required]
+    public int ProductId { get; set; }
+    [ForeignKey(nameof(ProductId))]
+    [ValidateNever]
+    public Product Product { get; set; }
+
+    public decimal Price { get; set; }
+}
