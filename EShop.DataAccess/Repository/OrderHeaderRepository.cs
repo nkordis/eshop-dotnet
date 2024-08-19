78368b60f59da8f8642a78ab713b08c9bed1793a
nkordis <nikoskordis@gmail.com>
2024-08-19 13:54:38 +0200

    chore: Add session ID to OrderHeader and update repository methods

--- EShop.DataAccess/Repository/OrderHeaderRepository.cs
+++ EShop.DataAccess/Repository/OrderHeaderRepository.cs
@@ -4,12 +4,39 @@ using EShop.Models.Models;
 
 namespace EShop.DataAccess.Repository;
 
-public class OrderHeaderRepository(ApplicationDbContext db) 
+public class OrderHeaderRepository(ApplicationDbContext db)
     : Repository<OrderHeader>(db), IOrderHeaderRepository
 {
     public void Update(OrderHeader obj)
     {
         db.Update(obj);
+    }
+
+    public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+    {
+        var orderFromDb = db.OrderHeaders.First(x => x.Id == id);
+        if (orderFromDb != null)
+        {
+            orderFromDb.OrderStatus = orderStatus;
+            if (!string.IsNullOrEmpty(paymentStatus))
+            {
+                orderFromDb.PaymentStatus = paymentStatus;
+            }
+        }
+    }
+
+    public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
+    {
+        var orderFromDb = db.OrderHeaders.First(x => x.Id == id);
+        if (!string.IsNullOrEmpty(sessionId))
+        {
+            orderFromDb.SessionId = sessionId;
+        }
+        if (!string.IsNullOrEmpty(paymentIntentId))
+        {
+            orderFromDb.PaymentIntentId = paymentIntentId;
+            orderFromDb.PaymentDate = DateTime.Now;
+        }
     }
 }
 
