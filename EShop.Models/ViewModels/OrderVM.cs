90555b7907408f6c83c1c60c4b3155830ac8563f
nkordis <nikoskordis@gmail.com>
2024-08-20 19:17:56 +0200

    feat: Implement order management with OrderController and OrderVM for admin order handling

---
+++ EShop.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,10 @@
+﻿
+using EShop.Models.Models;
+
+namespace EShop.Models.ViewModels;
+
+public class OrderVM
+{
+    public OrderHeader OrderHeader { get; set; }
+    public IEnumerable<OrderDetail> OrderDetails { get; set; }
+}
---
+++ EShop.Web/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using EShop.DataAccess.Repository.IRepository;
+using EShop.Models.Models;
+using EShop.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = SD.Role_Admin)]
+public class OrderController(IUnitOfWork unitOfWork) : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    #region API CALLS
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        List<OrderHeader> objOrderHeaders = [.. unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser")];
+        return Json(new { data = objOrderHeaders });
+    }
+
+    #endregion
+}
